6c66cd9befd3888a77bcc8c0ec3f233d19e9a816
Balkrishna Pandey <pandeybk@users.noreply.github.com>
2024-10-22 20:01:42 -0600

    datascience pipelines
    Signed-off-by: Balkrishna Pandey <pandeybk@users.noreply.github.com>

--- k8s/operators/minio/certs.sh
+++ k8s/operators/minio/certs.sh
@@ -2,9 +2,13 @@
 
 mkdir -p ./base/certs
 
-# Generate the private key and certificate
-openssl genrsa -out ./base/certs/tls.key 2048
-openssl req -x509 -new -nodes -key ./base/certs/tls.key -sha256 -days 365 -out ./base/certs/tls.crt -subj "/CN=minio/O=minio"
+# Define the domain for MinIO
+DOMAIN="minio-dspa-netsentinel.apps.cloud.xtoph152.dfw.ocp.run"
 
-# Optionally, you can copy the certificate to the CA bundle
+# Generate a private key
+openssl genrsa -out ./base/certs/tls.key 2048
+
+# Generate a self-signed certificate with the domain name
+openssl req -x509 -new -nodes -key ./base/certs/tls.key -sha256 -days 365 -out ./base/certs/tls.crt -subj "/CN=$DOMAIN"
 cp ./base/certs/tls.crt ./base/certs/ca-bundle.crt
+openssl x509 -in ./base/certs/tls.crt -text -noout
