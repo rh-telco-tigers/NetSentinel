d85643cb4ee4983d4eb9f3d6f7cea05d554d6d9b
Balkrishna Pandey <pandeybk@users.noreply.github.com>
2024-12-03 13:54:23 -0700

    test predictive model scripts
    Signed-off-by: Balkrishna Pandey <pandeybk@users.noreply.github.com>

--- scripts/test-predictive-model.sh
+++ scripts/test-predictive-model.sh
@@ -2,7 +2,7 @@
 
 # Namespace and route name
 NAMESPACE="multimodelserver"
-ROUTE_NAME="netsentinel"
+ROUTE_NAME="demo"
 
 # Fetch the route host dynamically
 ROUTE_HOST=$(oc get route $ROUTE_NAME -n $NAMESPACE -o jsonpath='{.spec.host}')
@@ -14,7 +14,7 @@ if [[ -z "$ROUTE_HOST" ]]; then
 fi
 
 # Construct the full URL
-FULL_URL="https://$ROUTE_HOST/v2/models/netsentinel/infer"
+FULL_URL="https://$ROUTE_HOST/v2/models/$ROUTE_NAME/infer"
 
 # Define the input data inline
 INPUT_DATA=$(cat <<EOF
@@ -22,13 +22,13 @@ INPUT_DATA=$(cat <<EOF
   "inputs": [
     {
       "name": "float_input",
-      "shape": [1, 47],
+      "shape": [1, 41],
       "datatype": "FP32",
       "data": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,
                1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0,
                2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0,
                3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0,
-               4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7]
+               4.1]
     }
   ]
 }
